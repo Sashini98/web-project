5aa441fe8f44f388a94e595384b18270b3bbc53a
Suvink <hello@suvink.me>
2020-01-20 21:12:57 +0530

    Modified edit user 2

--- web project/Edit_user2.php
+++ web project/Edit_user2.php
@@ -6,7 +6,7 @@ if(isset($_POST['update']))
 {
 	echo $_REQUEST['name'];
 	$sql2 = "UPDATE user SET name = '".$_POST['name']."',email = '".$_POST['email']."',telephone = '".$_POST['telephone']."',address = '".$_POST['address']."' WHERE id ='".$_POST['id']."'";
-	$result2 = mysqli_query($connection,$sql2);
+	$result2 = mysqli_query($conn,$sql2);
 	// $sql3 = "SELECT * FROM user WHERE id =".$_POST['id'];
 	// $result3 = mysqli_query($connection,$sql3);
 	// $row=mysqli_fetch_assoc($result3);

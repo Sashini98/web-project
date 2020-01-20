c744a19fdb302c249e34a556de731af1f1a2d193
Suvink <hello@suvink.me>
2020-01-20 21:30:25 +0530

    db

--- sdatabase.sql
+++ sdatabase.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jan 20, 2020 at 02:32 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Host: localhost
+-- Generation Time: Jan 20, 2020 at 03:59 PM
+-- Server version: 5.7.18
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,16 +28,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` varchar(256) NOT NULL,
   `name` varchar(256) NOT NULL,
   `email` varchar(256) NOT NULL,
   `telephone` int(10) NOT NULL,
   `address` varchar(256) NOT NULL,
   `account` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
+  `password` varchar(256) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -45,10 +43,20 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `telephone`, `address`, `account`, `password`) VALUES
-('a0011', 'aaaaaaaaaa', 'aaaaaaaaaaa', 1123456697, 'aaaaaaaaaaaa', 'admin', 'aaa'),
-('s0011', 'ssssss', 'ssssssss', 1124554785, 'ssssss', 'supplier', 'sss'),
-('c0011', 'cccccc', 'cccccc', 114567896, 'cccccccccc', 'customer', 'ccc'),
-('789', 'wwwwww', 'wwwwww', 123456789, 'qqqqqq', '1', '123');
+('a0011', 'My Dilhani', 'dgamhatha@gmail.com', 1123456697, 'aaaaaaaaaaaa', 'admin', 'aaa'),
+('s0011', 'Dilhani is Bae', 'ssssssss', 1124554785, 'ssssss', 'supplier', 'sss'),
+('c0011', 'Dilhani is mine', 'cccccc', 114567896, 'cccccccccc', 'customer', 'ccc'),
+('789', 'Dilhani is the cutest person on earth', 'wwwwww', 123456789, 'qqqqqq', '1', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
